c1feaa6d7598a2e4c526ccd875a4b9cadfd70f04
I5 <szigetvarimarkjanos@gmail.com>
2023-11-28 14:24:38 +0100

    save5

--- public/index.php
+++ public/index.php
@@ -103,5 +103,20 @@ if (mysqli_num_rows($result) > 0) {
     echo "No user found";
 }
 
+$sql = "SELECT * FROM helyezettek";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row["orszag"] . "<br>";
+        echo $row["arany"] . "<br>";
+        echo $row["ezust"] . "<br>";
+        echo $row["bronz"] . "<br>";
+    };
+} else {
+    echo "No user found";
+}
+
 mysqli_close($conn);
 
+?>
